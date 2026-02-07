70bdfdfc4551061148c957530bfcf13a6c52fa3e
Traver <traver.blumenfeld@mymail.champlain.edu>
2026-02-07 16:55:40 -0500

    Looking better

--- assets/shaders/blinnphong.fs
+++ assets/shaders/blinnphong.fs
@@ -23,9 +23,13 @@ uniform Light light;
 uniform Material material;
 
 vec3 blinnPhong(vec3 normal, vec3 frag_pos, Light light) {
+
+//float diffuse = max(dot(normal, light_direction), 0.0);
+//float specular = pow(max(dot(normal, halfway_direction), 0.0), material.shininess);
+
   vec3 viewDir = normalize(camera - frag_pos);
   vec3 lightDir = normalize(light.pos - frag_pos);
-  vec3 halfDir = normalize(viewDir + lightDir);
+  vec3 halfDir = normalize(lightDir + viewDir);
 
   float NDotL = max(dot(normal, lightDir), 0.0);
   float NDotH = max(dot(normal, halfDir), 0.0);
@@ -33,7 +37,7 @@ vec3 blinnPhong(vec3 normal, vec3 frag_pos, Light light) {
   vec3 diffuse = NDotL * material.diffuse;
   vec3 specular = pow(NDotH, material.shininess *128.0) * material.specular;
 
-  return (diffuse + specular) * light.color;
+  return (material.ambient + diffuse + specular) * light.color;
 }
 
 void main()
@@ -41,6 +45,6 @@ void main()
   vec3 norm = normalize(vs_normal);
   vec3 color = blinnPhong(norm, vs_position, light);
   vec3 objColor = norm * 0.5 + 0.5;
-  vec3 finalC = color*objColor * light.color;
+  vec3 finalC = color*objColor;
   FragColor = vec4(finalC, 1.0);
 }
