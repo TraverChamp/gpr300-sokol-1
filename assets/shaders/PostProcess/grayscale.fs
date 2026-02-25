faab6cc0238ce90f7d8d378514a9f9129ec6131b
Traver <traver.blumenfeld@mymail.champlain.edu>
2026-02-25 18:14:31 -0500

    Grayscale Coded, also removed effect function from the fragment shaders since we aren't using it

--- assets/shaders/PostProcess/grayscale.fs
+++ assets/shaders/PostProcess/grayscale.fs
@@ -9,12 +9,11 @@ in vec3 vs_position;
 in vec3 vs_normal;
 in vec2 vs_texcoord;
 
-vec3 effect() {
-  return normalize(vs_normal.rgb);
-}
+uniform sampler2D screenTexture;
 
 void main()
 {
-  vec3 color = effect();
-  FragColor = vec4(color, 1.0);
+  FragColor = texture(screenTexture, vs_texcoord);
+  float avg = (FragColor.r+FragColor.g+FragColor.b)/3.0;
+  FragColor = vec4(avg, avg, avg, 1.0);
 }
--- assets/shaders/PostProcess/inverse.fs
+++ assets/shaders/PostProcess/inverse.fs
@@ -9,12 +9,8 @@ in vec3 vs_position;
 in vec3 vs_normal;
 in vec2 vs_texcoord;
 uniform sampler2D screenTexture;
-vec3 effect() {
-  return normalize(vs_normal.rgb);
-}
 
 void main()
 {
-  vec3 color = effect();
   FragColor = vec4(vec3(1.0-texture(screenTexture, vs_texcoord)), 1.0);
 }
